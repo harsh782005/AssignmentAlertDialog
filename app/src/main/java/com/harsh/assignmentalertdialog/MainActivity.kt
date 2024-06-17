4ad11252382a1e710e0215af00af508795f6f34d
harsh782005 <harshdeepkaur453@gmail.com>
2024-06-17 17:17:38 +0530

    initial commit

--- app/src/main/java/com/harsh/assignmentalertdialog/MainActivity.kt
+++ app/src/main/java/com/harsh/assignmentalertdialog/MainActivity.kt
@@ -25,31 +25,30 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        binding?.btn?.setOnClickListener {
+        binding?.btnCalculate?.setOnClickListener {
             if (binding?.no?.text.toString().trim().isNullOrEmpty()) {
                 binding?.no?.error = "enter number"
             } else {
-
-                    var alertDialog = AlertDialog.Builder(this)
-                    alertDialog.setTitle("Perform calculation")
-                    alertDialog.setMessage("Add 10 to${binding?.no?.text}\nSub 10 to${binding?.no?.text}\nReset${binding?.no?.text}")
-                    alertDialog.setCancelable(false)
-                    alertDialog.setPositiveButton("Add 10", { _, _ ->
-                        binding?.no +=10;
-                        Toast.makeText(this, "postive button clicked", Toast.LENGTH_LONG).show()
-                    })
-                    alertDialog.setPositiveButton("Sub 10", { _, _ ->
-                        binding?.no-=10;
-                        Toast.makeText(this, "Negative button clicked", Toast.LENGTH_LONG).show()
-                    })
-                    alertDialog.setNeutralButton("Reset", { _, _ ->
-                        binding?.no?.setText({binding?.no?.text}==0)
-                        Toast.makeText(this, "Reset Clicked", Toast.LENGTH_LONG).show()
-                    })
-
+                var number = binding?.no?.text?.toString()?.toDouble() ?: 0.0
+                var alertDialog = AlertDialog.Builder(this)
+                alertDialog.setTitle("Perform calculation")
+                alertDialog.setMessage("Add 10 to${binding?.no?.text}\nSub 10 to${binding?.no?.text}\nReset${binding?.no?.text}")
+                alertDialog.setCancelable(false)
+                alertDialog.setPositiveButton("Add ${binding?.no?.text}") { _, _ ->
+                    binding?.no?.setText((number + number).toString())
+                    Toast.makeText(this, "postive button clicked", Toast.LENGTH_LONG).show()
                 }
-
+                alertDialog.setNegativeButton("Sub ${binding?.no?.text}") { _, _ ->
+                    binding?.no?.setText((number - number).toString())
+                    Toast.makeText(this, "Negative button clicked", Toast.LENGTH_LONG).show()
+                }
+                alertDialog.setNeutralButton("Reset") { _, _ ->
+                    binding?.no?.setText((0).toString())
+                    Toast.makeText(this, "Reset Clicked", Toast.LENGTH_LONG).show()
+                }
+                alertDialog.show()
             }
         }
     }
+}
 
